5968c7b74fa0798e1f16650f98f0804f9e529e30
FLORBELA FREITAS OLIVEIRA <21276327@CFP127ALUNO.LOCAL>
2022-01-19 16:49:56 -0300

    Aula 19/01 - iniciando

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-document.querySelector('h1').textContent='Leonid';
+//document.querySelector('h1').textContent='Leonid';
+
+//let name = "Fernando"
+//name = 123
+
+//console.log(name);
+//Usamos para testar o código
+
+//Definindo o h1 com o texto pego do input
+const title = document.getElementById('title');
+const name = document.getElementById('name');
+const red = document.getElementById('red');
+const blue = document.getElementById('blue');
+const green = document.getElementById('green');
+
+function changeTitleToRed() {
+    title.textContent = name.value;
+    title.classList.add('red')
+}
+function changeTitleToBlue() {
+    title.textContent = name.value;
+    title.classList.add('blue')
+}
+
+function changeTitleToGreen() {
+    title.textContent = name.value;
+    title.classList.add('green')
+}
+
+red.addEventListener('click', changeTitleToRed);
+blue.addEventListener('click', changeTitleToBlue);
+green.addEventListener('click', changeTitleToGreen);
